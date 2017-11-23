041bdd408c10d09e0d3671200e3a8967de188ac1
tsikalenko <32128038+tsikalenko@users.noreply.github.com>
2017-11-23 19:51:06 +0200

    Update Main.java

--- L10Translater/src/com/gmail/tsikalenko/nikita/Main.java
+++ L10Translater/src/com/gmail/tsikalenko/nikita/Main.java
@@ -5,12 +5,15 @@ import java.io.File;
 public class Main {
 	public static void main(String[] args) {
 		Dictionary dictionary = new Dictionary("English - Russian");
-		dictionary.add("hello", "привет");
-		dictionary.add("world", "мир");
+		dictionary.add("hello", "ГЇГ°ГЁГўГҐГІ");
+		dictionary.add("world", "Г¬ГЁГ°");
 		
 		File in = new File("english.txt");
 		File out = new File("russian.txt");
 		
 		Translater.translate(in, out, dictionary);
+		
+		File fileDictionary = new File("Dictionary.txt"); 
+		FileWorker.saveObject(dictionary, fileDictionary);
 	}
 }
